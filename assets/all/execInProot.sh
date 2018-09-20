6d9ca6851c5f5979f5d602369ecaf3e408734fc4
Corbin Champion <corbinlc@gmail.com>
2018-09-20 13:47:54 -0700

    workaround missing proc/stat file

--- assets/all/execInProot.sh
+++ assets/all/execInProot.sh
@@ -2,5 +2,22 @@
 
 $ROOT_PATH/support/busybox clear
 
+if [[ ! -r /dev/ashmem ]] ; then
+	EXTRA_BINDINGS+=" -b $ROOTFS_PATH/tmp:/dev/ashmem " 
+fi
+if [[ ! -r /dev/shm ]] ; then
+	EXTRA_BINDINGS+=" -b $ROOTFS_PATH/tmp:/dev/shm " 
+fi
+if [[ ! -r /proc/stat ]] ; then
+	numProc="$($ROOT_PATH/support/busybox grep rocessor /proc/cpuinfo)"
+	numProc="${numProc: -1}"
+	if [[ "$numProc" -le "3" ]] 2>/dev/null ; then
+		addfbindprocstat4
+		EXTRA_BINDINGS+=" -b $ROOT_PATH/support/stat4:/proc/stat " 
+	else
+		EXTRA_BINDINGS+=" -b $ROOT_PATH/support/stat8:/proc/stat " 
+	fi
+fi
+
 #launch PRoot
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin PROOT_TMP_DIR=$ROOTFS_PATH/support/ $ROOT_PATH/support/proot -r $ROOTFS_PATH -v $PROOT_DEBUG_LEVEL -p -H -0 -l -L -b /sys -b /dev -b /proc -b /data -b /mnt -b /proc/mounts:/etc/mtab -b /:/host-rootfs -b $ROOTFS_PATH/support/:/support -b $ROOTFS_PATH/support/nosudo:/usr/local/bin/sudo -b $ROOTFS_PATH/support/userland_profile.sh:/etc/profile.d/userland_profile.sh -b $ROOTFS_PATH/support/ld.so.preload:/etc/ld.so.preload $EXTRA_BINDINGS $@
