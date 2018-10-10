8309654bbdfc5455f6a432010fbfde1cf1570884
Matthew Tighe <matthewdtighe@gmail.com>
2018-10-10 15:14:11 -0700

    Paid apps. (#66)
    * Rename applications directory to apps.
    
    * Apps paid (#65)
    
    * add zork and adventure
    
    * added error checking to game scripts
    
    * add userland logo for IF games for now
    
    * add firefox as an app
    
    * dont forget apt update
    
    * fix package name for firefox-esr
    
    * fix small issues with how apps run/exit
    
    * add exit to end of adventure script
    
    * dont use octave gui and do use gnuplot
    
    * install the recommended packages for octave
    
    * install all recommended packages, but only for gnuplot
    
    * small fix to not install gnuplot twice
    
    * switch to gnuplot-x11
    
    * allow the gui to be used when appropriate for octave
    
    * add trailing exit
    
    * first pass at adding R
    
    * first pass at gnuplot as an app
    
    * add gnuplot as an app

--- apps/octave/octave.sh
+++ apps/octave/octave.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 SCRIPT_PATH=$(realpath ${BASH_SOURCE})
-
 sudo rm -f $SCRIPT_PATH
 
 if [ ! -f /support/.octave_app_install_passed ] || [ ! -f /usr/bin/octave ]; then
    rm -f /support/.octave_app_install_passed 
    sudo apt-get update
-   sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install octave less octave-control octave-financial octave-io octave-missing-functions octave-odepkg octave-optim octave-signal octave-specfun octave-statistics octave-symbolic octave-image
+   sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install octave less octave-control octave-financial octave-io octave-missing-functions octave-odepkg octave-optim octave-signal octave-specfun octave-statistics octave-symbolic octave-image gnuplot-x11 pstoedit
    if [[ $? == 0 ]]; then
+      echo "graphics_toolkit('gnuplot')" > ~/.octaverc
       touch /support/.octave_app_install_passed
    else
       read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
@@ -17,3 +17,4 @@ fi
 if [ -f /support/.octave_app_install_passed ]; then
    /usr/bin/octave
 fi
+exit
