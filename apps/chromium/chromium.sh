85ecc92e477c7266ad0234b992090e428f5cc845
Matt Ferrero <mattaferrero@gmail.com>
2018-10-15 11:57:53 -0400

    fixed bash typos

--- apps/chromium/chromium.sh
+++ apps/chromium/chromium.sh
@@ -3,13 +3,13 @@
 SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 sudo rm -f $SCRIPT_PATH
 
-if [ ! f /usr/bin/chromium-browser ]; then
+if [ ! -f /usr/bin/chromium-browser ]; then
 	sudo apt-get update
 	sudo DEBIAN_FRONTEND=noninteractive install chromium chromium-l10n
 	hash -r
 fi
 
-if [[ $?!= 0 ]]; then
+if [[ $? != 0 ]]; then
 	read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
    	exit
 fi
