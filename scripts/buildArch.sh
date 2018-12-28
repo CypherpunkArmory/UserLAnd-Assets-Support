4fc4d2e8ebfa473ac757306b94335e0727015020
Corbin Champion <corbinlc@gmail.com>
2018-12-27 16:43:34 -0800

    add libcrypto

--- scripts/buildArch.sh
+++ scripts/buildArch.sh
@@ -58,5 +58,6 @@ sudo PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH dropbear
 cp /data/data/com.termux/files/usr/bin/dbclient $ARCH_DIR/dbclient
 cp /data/data/com.termux/files/usr/lib/libtermux-auth.so $ARCH_DIR/libtermux-auth.so
 cp /data/data/com.termux/files/usr/lib/libutil.so $ARCH_DIR/libutil.so
+cp /data/data/com.termux/files/usr/lib/libcrypto.so.1.1 $ARCH_DIR/libcrypto.so.1.1
 #sudo PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH busybox
 #cp /data/data/com.termux/files/usr/bin/busybox $ARCH_DIR/busybox
