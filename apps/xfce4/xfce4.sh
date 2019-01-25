5df8df79b0ca5e9ee761dd46a2bb6ce6f99a6eb5
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-01-25 13:25:40 +1300

    Update xfce4.sh

--- apps/xfce4/xfce4.sh
+++ apps/xfce4/xfce4.sh
@@ -11,5 +11,8 @@ fi
 if [[ $? != 0 ]]; then
    read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
    exit
+
+startxfce4
+
 fi
 
