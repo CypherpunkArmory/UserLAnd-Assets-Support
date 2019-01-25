29b24a1ba9f3847b32a5f1e213ec0c190a753889
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-01-25 13:41:20 +1300

    Update mate.sh

--- apps/mate/mate.sh
+++ apps/mate/mate.sh
@@ -3,9 +3,9 @@
 SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 sudo rm -f $SCRIPT_PATH
 
-if [ ! -f /usr/bin/gmate ]; then
+if [ ! -f /usr/bin/user/bin/user/bin/mate ]; then
    sudo apt-get update
-   sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install mate
+   sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install mate-desktop-environment
 fi
 
 if [[ $? != 0 ]]; then
