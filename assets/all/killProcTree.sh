f85a49166c62381c3166a2d9259b57cb62bfcac6
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-02-26 12:45:56 -0800

    Use  instead of relative paths for busybox.

--- assets/all/killProcTree.sh
+++ assets/all/killProcTree.sh
@@ -1,11 +1,11 @@
 #!/data/data/tech.ula/files/support/busybox sh
 
 dokill() {
-    for cpid in $(../support/busybox ps -o ppid,pid | ../support/busybox awk -v pid="$1" '$1 == pid { print $2 }') 
+    for cpid in $($ROOT_PATH/support/busybox ps -o ppid,pid | $ROOT_PATH/support/busybox awk -v pid="$1" '$1 == pid { print $2 }') 
     do
         dokill $cpid
     done
-    echo "killing $1 $(../support/busybox ps -o pid,comm | ../support/busybox awk -v pid="$1" '$1 == pid { print $2 }')"
+    echo "killing $1 $($ROOT_PATH/support/busybox ps -o pid,comm | $ROOT_PATH/support/busybox awk -v pid="$1" '$1 == pid { print $2 }')"
     kill -9 $1 > /dev/null 2>&1
 }
 
