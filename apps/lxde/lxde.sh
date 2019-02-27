ae04986251524b8cf794931630131821c461a48a
Corbin Champion <corbinlc@gmail.com>
2019-02-27 14:38:44 -0800

    install recommended packages

--- apps/lxde/lxde.sh
+++ apps/lxde/lxde.sh
@@ -5,7 +5,7 @@ sudo rm -f $SCRIPT_PATH
 
 if [ ! -f /usr/bin/startlxde ]; then
    sudo apt-get update
-   sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install lxde-core
+   sudo DEBIAN_FRONTEND=noninteractive apt-get -y install lxde
    if [[ $? != 0 ]]; then
       read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
       exit
