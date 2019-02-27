363f950996ac6ea5a03f079935a18f77c8994ed6
Corbin Champion <corbinlc@gmail.com>
2019-02-26 16:34:31 -0800

    first pass at code for de swapping

--- apps/lxde/lxde.sh
+++ apps/lxde/lxde.sh
@@ -5,14 +5,19 @@ sudo rm -f $SCRIPT_PATH
 
 if [ ! -f /usr/bin/lxde ]; then
    sudo apt-get update
-   sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install lxde
-fi
+   sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install lxde-core
+   if [[ $? != 0 ]]; then
+      read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
+      exit
+   fi
+fi 
 
-if [[ $? != 0 ]]; then
-   read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
-   exit
+if grep -q "Session=LXDE" ~/.dmrc; then
+   echo "already setup"
+else
+   echo "[Desktop]" > ~/.dmrc
+   echo "Session=LXDE" >> ~/.dmrc
+   read -rsp $'Stop and then restart the app to see the new desktop.\n' -n1 key
 fi
-
-startlxde
 
 exit
