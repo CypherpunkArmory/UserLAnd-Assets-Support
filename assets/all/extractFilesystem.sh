74d4e7e8d3360c3cce671d3dac968f19f64c4c29
Corbin Champion <corbinlc@gmail.com>
2019-02-28 13:45:20 -0800

    exclude files from extraction

--- assets/all/extractFilesystem.sh
+++ assets/all/extractFilesystem.sh
@@ -5,7 +5,7 @@ if [ ! -f /support/rootfs.tar.gz ]; then
    /support/common/busybox_static rm -f /support/rootfs.tar.gz.part*
 fi
 
-/support/common/busybox_static tar -xzvf /support/rootfs.tar.gz -C /
+/support/common/busybox_static tar -tzf /support/rootfs.tar.gz | /support/common/busybox_static grep -vE '^(sys|dev|proc|data|mnt|host-rootfs|support|sdcard|etc/mtab|usr/local/bin/sudo|etc/profile.d/userland_profile.sh|etc/ld.so.preload)' | /support/common/busybox_static xargs /support/common/busybox_static tar -xzvf /support/rootfs.tar.gz -C /
 
 if [[ $? == 0 ]]; then
 	/support/common/addNonRootUser.sh
