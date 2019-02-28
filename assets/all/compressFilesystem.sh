3346a99e9b1466c87a7024693673ff4c58157578
Corbin Champion <corbinlc@gmail.com>
2019-02-28 10:58:07 -0800

    add full path to busybox

--- assets/all/compressFilesystem.sh
+++ assets/all/compressFilesystem.sh
@@ -4,4 +4,4 @@ if [[ -z "${TAR_PATH}" ]]; then
   TAR_PATH="/sdcard/rootfs.tar.gz"
 fi
 
-/support/common/busybox_static find . -maxdepth 1 -not -name '.' -and -not -name 'sys' -and -not -name 'dev' -and -not -name 'proc' -and -not -name 'data' -and -not -name 'mnt' -and -not -name 'host-rootfs' -and -not -name 'support' | busybox_static xargs busybox_static tar -czvf $TAR_PATH
+/support/common/busybox_static find / -maxdepth 1 -not -name '.' -and -not -name 'sys' -and -not -name 'dev' -and -not -name 'proc' -and -not -name 'data' -and -not -name 'mnt' -and -not -name 'host-rootfs' -and -not -name 'support' | /support/common/busybox_static xargs /support/common/busybox_static tar -czvf $TAR_PATH
