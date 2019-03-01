b0f02c14e60726e54ecbd12fdff992d3af4b168d
Corbin Champion <corbinlc@gmail.com>
2019-03-01 10:28:06 -0800

    untar to a temp directory so we can delete some files before putting it in place

--- assets/all/extractFilesystem.sh
+++ assets/all/extractFilesystem.sh
@@ -5,7 +5,17 @@ if [ ! -f /support/rootfs.tar.gz ]; then
    /support/common/busybox_static rm -f /support/rootfs.tar.gz.part*
 fi
 
-/support/common/busybox_static tar -tzf /support/rootfs.tar.gz | /support/common/busybox_static grep -vE '^(sys|dev|proc|data|mnt|host-rootfs|support|sdcard|etc/mtab|usr/local/bin/sudo|etc/profile.d/userland_profile.sh|etc/ld.so.preload)' | /support/common/busybox_static xargs /support/common/busybox_static tar -xzvf /support/rootfs.tar.gz -C /
+#only untar directories we might use and put it in a temp directory
+/support/common/busybox_static mkdir /tartmp
+/support/common/busybox_static tar -tzf /support/rootfs.tar.gz | /common/busybox_static grep '^[^/]*/$' | /support/common/busybox_static grep -vE '^(sys|dev|proc|data|mnt|host-rootfs|support|sdcard)' | /support/common/busybox_static xargs /support/common/busybox_static tar -xzvf /support/rootfs.tar.gz -C /tartmp
+#delete specific files we are going to replace with bindings
+/support/common/busybox_static rm -rf /tartmp/etc/mtab
+/support/common/busybox_static rm -rf /tartmp/usr/local/bin/sudo
+/support/common/busybox_static rm -rf /tartmp/etc/profile.d/userland_profile.sh
+/support/common/busybox_static rm -rf /tartmp/etc/ld.so.preload
+#move it to / and delete the temp directory
+/support/common/busybox_static mv /tartmp/* /
+/support/common/busybox_static rm -rf /tartmp
 
 if [[ $? == 0 ]]; then
 	/support/common/addNonRootUser.sh
