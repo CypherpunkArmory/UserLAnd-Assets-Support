d7bbb2a0242dc39c0fcc3cbe652293723fa77279
EnderNightLord <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2019-04-13 16:17:57 +1200

    Update idle3.sh

--- apps/idle3/idle3.sh
+++ apps/idle3/idle3.sh
@@ -3,7 +3,7 @@
 SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 sudo rm -f $SCRIPT_PATH
 
-if [ ! -f /usr/bin/idle3 ]; then
+if [ ! -f /usr/bin/idle ]; then
    sudo apt-get update
    sudo DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install idle3
 fi
