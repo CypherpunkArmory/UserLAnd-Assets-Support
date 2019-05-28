ee89b106de0194a14fc203073f286fa19e4dd7fd
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-05-28 12:39:01 -0700

    Update assets to use LIB_PATH and remove bundled assets from lists.

--- assets/all/isServerInProcTree.sh
+++ assets/all/isServerInProcTree.sh
@@ -1,8 +1,8 @@
 #!/data/data/tech.ula/files/support/busybox sh
 
 checkForServer() {
-    echo "checking $1 $($ROOT_PATH/support/busybox ps -o pid,comm | $ROOT_PATH/support/busybox awk -v pid="$1" '$1 == pid { print $2 }')"
-    case "$($ROOT_PATH/support/busybox ps -o pid,comm | $ROOT_PATH/support/busybox awk -v pid="$1" '$1 == pid { print $2 }')" in
+    echo "checking $1 $($LIB_PATH/busybox ps -o pid,comm | $LIB_PATH/busybox awk -v pid="$1" '$1 == pid { print $2 }')"
+    case "$($LIB_PATH/busybox ps -o pid,comm | $LIB_PATH/busybox awk -v pid="$1" '$1 == pid { print $2 }')" in
         *dropbear*)
             echo "found dropbear"
             exit 0
@@ -17,7 +17,7 @@ checkForServer() {
             ;;
     esac
     
-    for cpid in $($ROOT_PATH/support/busybox ps -o ppid,pid | $ROOT_PATH/support/busybox awk -v pid="$1" '$1 == pid { print $2 }') 
+    for cpid in $($LIB_PATH/busybox ps -o ppid,pid | $LIB_PATH/busybox awk -v pid="$1" '$1 == pid { print $2 }') 
     do
         checkForServer $cpid
     done
