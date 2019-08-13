70b093b1b2c3d05b5a1136945ab5a94a22793039
Corbin Champion <corbinlc@gmail.com>
2019-08-13 15:24:14 -0700

    first pass at fixing new seccomp filtering related issues

--- input/main.sh
+++ input/main.sh
@@ -44,6 +44,8 @@ mkdir -p $ARCH_DIR
 if [ ! -d $PROOT_DIR ]
 then
     git clone https://github.com/CypherpunkArmory/proot.git $PROOT_DIR
+    cd $PROOT_DIR
+    git checkout -b even-more-seccomp
 fi
 
 if [ ! -d $TERMUX_PACKAGES_DIR ]
