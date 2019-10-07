e1800c3ac1442888fbd8dc97da910d28b1ffc754
Corbin Champion <corbinlc@gmail.com>
2019-10-07 13:19:02 -0700

    first pass at merge with termux master

--- input/main.sh
+++ input/main.sh
@@ -46,7 +46,7 @@ then
     git clone https://github.com/CypherpunkArmory/proot.git $PROOT_DIR
     cd $PROOT_DIR
     git fetch
-    git checkout void-sigsys 
+    git checkout merge-termux-again 
 fi
 
 if [ ! -d $TERMUX_PACKAGES_DIR ]
