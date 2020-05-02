c3c2ac2081fd70e10018a439f5c6e29e912b5ea2
corbinlc <corbinlc@gmail.com>
2020-05-02 15:09:15 -0700

    Update main.sh

--- input/main.sh
+++ input/main.sh
@@ -46,7 +46,7 @@ then
     git clone https://github.com/CypherpunkArmory/proot.git $PROOT_DIR
     cd $PROOT_DIR
     git fetch
-    git checkout merge-termux-again 
+    git checkout master
 fi
 
 if [ ! -d $TERMUX_PACKAGES_DIR ]
