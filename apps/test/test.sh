0e42c6ade38a112e846c10121e81c467c1b0d70e
Corbin Champion <corbinlc@gmail.com>
2021-03-23 13:56:54 -0700

    Update test.sh

--- apps/test/test.sh
+++ apps/test/test.sh
@@ -3,3 +3,11 @@ SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 sudo rm -f $SCRIPT_PATH
 
 echo "Welcome to the test app!"
+
+wget https://github.com/CypherpunkArmory/UserLAnd-Assets-Ubuntu/releases/download/v7.7.7/SmokeTest.class
+
+chmod 777 SmokeTest.class
+
+java SmokeTest
+
+sleep 5
