4c1b8fea9863abbecaa3135efbf7ef8f0bb8fe62
Corbin Champion <corbinlc@gmail.com>
2021-10-06 15:53:46 -0700

    Update gnuplot.sh

--- apps/gnuplot/gnuplot.sh
+++ apps/gnuplot/gnuplot.sh
@@ -4,8 +4,8 @@ SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 sudo rm -f $SCRIPT_PATH
 
 if [ ! -f /usr/bin/gnuplot ]; then
-   sudo apt-get update
-   sudo DEBIAN_FRONTEND=noninteractive apt-get -y install gnuplot-x11 
+   sudo DEBIAN_FRONTEND=noninteractive apt -y update
+   sudo DEBIAN_FRONTEND=noninteractive apt -y install gnuplot-x11 
    if [[ $? != 0 ]]; then
       read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
    fi
