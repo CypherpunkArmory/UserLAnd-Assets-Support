6f316e60cccad46f2e6c1fe584ea200e5955e896
Corbin Champion <corbinlc@gmail.com>
2021-11-10 10:25:37 -0800

    update paths

--- input/main.sh
+++ input/main.sh
@@ -52,17 +52,13 @@ fi
 
 if [ ! -d $TERMUX_PACKAGES_DIR ]
 then
-    git clone https://github.com/termux/termux-packages.git $TERMUX_PACKAGES_DIR
+    git clone https://github.com/CypherpunkArmory/termux-packages.git $TERMUX_PACKAGES_DIR
     cd $TERMUX_PACKAGES_DIR
-    #git checkout -b userland 7f9d1ad9243cdcc0d477f8495091fe2bb9444569
     git fetch
     git checkout android-5
     git checkout master -- packages/ncurses/build.sh
     scripts/setup-ubuntu.sh
     scripts/setup-android-sdk.sh
-    sed -i 's/TERMUX_PKG_SRCDIR/PROOT_DIR/g' packages/proot/build.sh
-    sed -i 's/make V=1/make clean\n        make V=1/g' packages/proot/build.sh
-    sed -i 's/0d98a1a961aab523c9dc547e315e1d79e887dea575426ff03567e455fc0b66b4/ae31ecb3c6e9ff3154cb7a55f017090448f88482f0e94ac927c0c67a1f33b9cf/g' packages/ca-certificates/build.sh
 else
     cd $TERMUX_PACKAGES_DIR
 fi
@@ -84,8 +80,6 @@ PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH proot
 cp /data/data/com.termux/files/usr/bin/proot $ARCH_DIR/proot.a10
 cp /data/data/com.termux/files/usr/libexec/proot/loader $ARCH_DIR/loader.a10
 cp /data/data/com.termux/files/usr/libexec/proot/loader32 $ARCH_DIR/loader32.a10
-
-taskset 0x1 apt -y install openjdk-8-jre
 
 PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libtool
 PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libogg
