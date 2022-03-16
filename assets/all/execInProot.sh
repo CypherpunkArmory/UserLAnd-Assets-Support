202a44ac6fb696763586643aa219860fdfe5745f
Corbin Champion <corbinlc@gmail.com>
2022-03-16 10:14:07 -0700

    Start pulse audio daemon outside of rootfs

--- assets/all/execInProot.sh
+++ assets/all/execInProot.sh
@@ -52,6 +52,24 @@ fi
 PROOT_VER=$($LIB_PATH/busybox cat $ROOTFS_PATH/support/.proot_version)
 PROOT="$LIB_PATH/proot$PROOT_VER"
 
+#launch PulseAudio
+export PULSE_SCRIPT="$LIB_PATH/default.pa"
+export PULSE_CONFIG="$LIB_PATH/daemon.conf"
+export PULSE_DLPATH="$LIB_PATH/"
+export XDG_DATA_HOME="$LIB_PATH/"
+export XDG_CONFIG_HOME="$LIB_PATH/"
+export XDG_STATE_HOME="$LIB_PATH/"
+export TMPDIR="$LIB_PATH/"
+$LIB_PATH/pulseaudio --log-level=4 --log-target=stderr --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 &> $ROOTFS_PATH/support/paserv.log
+unset PULSE_SCRIPT
+unset PULSE_CONFIG
+unset PULSE_DLPATH
+unset XDG_DATA_HOME
+unset XDG_CONFIG_HOME
+unset XDG_STATE_HOME
+unset TMPDIR
+export PULSE_SERVER="127.0.0.1"
+
 #launch PRoot
 unset LD_PRELOAD
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin PROOT_TMP_DIR=$ROOTFS_PATH/support/ PROOT_LOADER=$LIB_PATH/loader PROOT_LOADER_32=$LIB_PATH/loader32 $PROOT -r $ROOTFS_PATH -v $PROOT_DEBUG_LEVEL -p --sysvipc -H -0 -l -L -b /sys -b /dev -b /proc -b /data -b /mnt -b /proc/mounts:/etc/mtab -b /:/host-rootfs -b $ROOTFS_PATH/support/:/support -b $ROOTFS_PATH/support/nosudo:/usr/local/bin/sudo -b $ROOTFS_PATH/support/userland_profile.sh:/etc/profile.d/userland_profile.sh -b $ROOTFS_PATH/support/ld.so.preload:/etc/ld.so.preload -b $ROOT_PATH/support:/support/common $EXTRA_BINDINGS $@
