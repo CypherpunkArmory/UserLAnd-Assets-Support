9929e30deb84d40d53d3bc6bb8808b565a9fd8a5
pkristof-dev <114728004+pkristof-dev@users.noreply.github.com>
2023-05-24 11:44:22 +0000

    Updated code-server install script

--- apps/code-server/code-server.sh
+++ apps/code-server/code-server.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#! /bin/bash
 
-sudo apt-get update
-sudo curl -fsSL https://code-server.dev/install.sh | sh
+SCRIPT_PATH=$(realpath ${BASH_SOURCE})
+sudo rm -f $SCRIPT_PATH
+
+if [ ! -f /usr/bin/code-server ]; then
+    sudo apt update
+    sudo apt install ca-certificates
+    sudo curl -fsSL https://code-server.dev/install.sh | sh
+fi
+
+if [[ $? != 0 ]]; then
+   read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
+   exit
+fi
+
+code-server
+
+exit
