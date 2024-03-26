e82f7bc922fe152ddccf1a1ad9ebf316ef0bd149
Naveen Cherukupalle <naveen@debian-BULLSEYE-live-builder-AMD64>
2024-03-26 10:09:42 +0530

    Onboarding paninidhpc

---
+++ apps/paninidhpc/paninidhpc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_PATH=$(realpath ${BASH_SOURCE})
+
+sudo rm -f $SCRIPT_PATH
+
+sudo apt update && sudo apt install -y curl ca-certificates
+
+curl -sSL https://code.swecha.org/panini-dhpc/panini-cluster/-/raw/main/android.sh | sudo bash -s 1 Swecha_Android
+
+curl -sSL https://code.swecha.org/panini-dhpc/panini-cluster/-/raw/main/android_start.sh | sudo bash
