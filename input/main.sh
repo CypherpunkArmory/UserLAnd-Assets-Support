fd9c266cad9d780958e5b86e0d8efd8194bd3d71
Corbin Champion <corbinlc@gmail.com>
2025-09-26 12:27:45 -0700

    first attempt at adding sound support like in andacious app

--- input/main.sh
+++ input/main.sh
@@ -52,17 +52,12 @@ fi
 
 if [ ! -d $TERMUX_PACKAGES_DIR ]
 then
-    git clone https://github.com/termux/termux-packages.git $TERMUX_PACKAGES_DIR
+    git clone https://github.com/CypherpunkArmory/termux-packages.git $TERMUX_PACKAGES_DIR
     cd $TERMUX_PACKAGES_DIR
-    #git checkout -b userland 7f9d1ad9243cdcc0d477f8495091fe2bb9444569
     git fetch
-    #git checkout android-5
-    git checkout -b userland cea1b34f262d540c2d74ada6bfd5f2cc3a8a36db
+    git checkout android-5
     scripts/setup-ubuntu.sh
     scripts/setup-android-sdk.sh
-    sed -i 's/TERMUX_PKG_SRCDIR/PROOT_DIR/g' packages/proot/build.sh
-    sed -i 's/make V=1/make clean\n        make V=1/g' packages/proot/build.sh
-    sed -i 's/c1fd9b235896b1094ee97bfb7e042f93530b5e300781f59b45edf84ee8c75000/adf770dfd574a0d6026bfaa270cb6879b063957177a991d453ff1d302c02081f/g' packages/ca-certificates/build.sh
 else
     cd $TERMUX_PACKAGES_DIR
 fi
@@ -84,6 +79,45 @@ PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH proot
 cp /data/data/com.termux/files/usr/bin/proot $ARCH_DIR/proot.a10
 cp /data/data/com.termux/files/usr/libexec/proot/loader $ARCH_DIR/loader.a10
 cp /data/data/com.termux/files/usr/libexec/proot/loader32 $ARCH_DIR/loader32.a10
+
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libtool
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libogg
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libflac
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libc++
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libandroid-glob
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libvorbis
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libsndfile 
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH libsoxr
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH speexdsp
+PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH pulseaudio
+
+ls /data/data/com.termux/files/usr/bin/
+ls /data/data/com.termux/files/usr/lib/
+ls /data/data/com.termux/files/usr/libexec/
+ls /data/data/com.termux/files/usr/etc/pulse/
+ls /data/data/com.termux/files/usr/lib/pulse-13.0/
+ls /data/data/com.termux/files/usr/lib/pulse-13.0/modules/
+
+cp /data/data/com.termux/files/usr/libexec/pulseaudio $ARCH_DIR/pulseaudio
+cp /data/data/com.termux/files/usr/libexec/pactl $ARCH_DIR/pactl
+cp /data/data/com.termux/files/usr/bin/pulseaudio $ARCH_DIR/pulseaudio-script
+cp /data/data/com.termux/files/usr/lib/libpulsecore-13.0.so $ARCH_DIR/libpulsecore-13.0.so
+cp /data/data/com.termux/files/usr/lib/libpulsecommon-13.0.so $ARCH_DIR/libpulsecommon-13.0.so
+cp /data/data/com.termux/files/usr/lib/libpulse.so $ARCH_DIR/libpulse.so
+cp /data/data/com.termux/files/usr/lib/libltdl.so $ARCH_DIR/libltdl.so
+cp /data/data/com.termux/files/usr/lib/libandroid-glob.so $ARCH_DIR/libandroid-glob.so
+cp /data/data/com.termux/files/usr/lib/libiconv.so $ARCH_DIR/libiconv.so
+cp /data/data/com.termux/files/usr/lib/libsndfile.so $ARCH_DIR/libsndfile.so
+cp /data/data/com.termux/files/usr/lib/libspeexdsp.so $ARCH_DIR/libspeexdsp.so
+cp /data/data/com.termux/files/usr/lib/libsoxr.so.0 $ARCH_DIR/libsoxr.so.0
+cp /data/data/com.termux/files/usr/lib/libogg.so $ARCH_DIR/libogg.so
+cp /data/data/com.termux/files/usr/lib/libvorbis.so $ARCH_DIR/libvorbis.so
+cp /data/data/com.termux/files/usr/lib/libvorbisenc.so $ARCH_DIR/libvorbisenc.so
+cp /data/data/com.termux/files/usr/lib/libFLAC.so $ARCH_DIR/libFLAC.so
+cp /data/data/com.termux/files/usr/etc/pulse/daemon.conf $ARCH_DIR/daemon.conf
+cp /data/data/com.termux/files/usr/etc/pulse/default.pa $ARCH_DIR/default.pa
+cp /data/data/com.termux/files/usr/lib/pulse-13.0/modules/* $ARCH_DIR/
+
 #PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH openssl
 #PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH termux-auth
 #PROOT_DIR=$PROOT_DIR ./build-package.sh -f -a $TERMUX_ARCH dropbear
