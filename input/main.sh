b8ba93d53fa8107e0c1be473001dcdbd259c3cfa
Corbin Champion <corbinlc@gmail.com>
2026-05-22 16:36:45 -0700

    Revert last two commits

--- input/main.sh
+++ input/main.sh
@@ -47,7 +47,7 @@ then
     git clone https://github.com/CypherpunkArmory/proot.git $PROOT_DIR
     cd $PROOT_DIR
     git fetch
-    git checkout no_orphans
+    git checkout droid_files 
 fi
 
 if [ ! -d $TERMUX_PACKAGES_DIR ]
