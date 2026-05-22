643aa5869015f96fa88f0a0a9afcf93d19d4b32e
Corbin Champion <corbinlc@gmail.com>
2026-05-22 13:17:52 -0700

    try build of no_orphans for the first time

--- input/main.sh
+++ input/main.sh
@@ -47,7 +47,7 @@ then
     git clone https://github.com/CypherpunkArmory/proot.git $PROOT_DIR
     cd $PROOT_DIR
     git fetch
-    git checkout droid_files 
+    git checkout no_orphans
 fi
 
 if [ ! -d $TERMUX_PACKAGES_DIR ]
